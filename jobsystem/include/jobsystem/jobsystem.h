b2ee51e41b05571ad74c469bb253ece5c2a06da4
Davide Bacchet <davide.bacchet@gmail.com>
2019-04-06 22:40:59 -0700

    better opaque types

--- jobsystem/include/jobsystem/jobsystem.h
+++ jobsystem/include/jobsystem/jobsystem.h
@@ -6,16 +6,16 @@ extern "C" {
 
 #include "job.h"
 
-typedef struct atomic_counter_handle_ atomic_counter_handle;
-typedef struct job_manager_t_ job_manager_t;
+typedef struct atomic_counter_t* atomic_counter_handle;
+typedef struct job_manager_t* job_manager_handle;
 
-job_manager_t* create_job_manager(uint32_t num_worker_threads, uint32_t num_fibers, uint32_t fiber_stack_size);
-void destroy_job_manager(job_manager_t **job_manager_t);
+job_manager_handle create_job_manager(uint32_t num_worker_threads, uint32_t num_fibers, uint32_t fiber_stack_size);
+void destroy_job_manager(job_manager_handle jm);
 
-void run_jobs(job_manager_t *jm, job_t *jobs, uint32_t num_jobs, atomic_counter_handle *counter);
-void wait_for_counter(job_manager_t *jm, atomic_counter_handle *counter, uint32_t value);
+void run_jobs(job_manager_handle jm, job_t *jobs, uint32_t num_jobs, atomic_counter_handle counter);
+void wait_for_counter(job_manager_handle jm, atomic_counter_handle counter, uint32_t value);
 
-void stop_job_manager(job_manager_t *job_manager_t);
+void stop_job_manager(job_manager_handle job_manager_handle);
 
 #ifdef __cplusplus
 }
--- jobsystem/src/jobsystem.c
+++ jobsystem/src/jobsystem.c
@@ -8,31 +8,31 @@
 #include <stdbool.h>
 #include <stdatomic.h>
 
-struct atomic_counter_handle_ {
-    atomic_int *counter;
+struct atomic_counter_t {
+    atomic_int counter;
 };
-typedef struct atomic_counter_handle_ atomic_counter_handle;
+typedef struct atomic_counter_t* atomic_counter_handle;
 
 // the job_ manager_t struct contains all the data structures needed to schedule and synchronize jobs
-struct job_manager_t_ {
+struct job_manager_t {
     thrd_t *threads;
     size_t num_threads;
     atomic_bool stop_threads;
 };
-typedef struct job_manager_t_ job_manager_t;
+typedef struct job_manager_t* job_manager_handle;
 
 // /////////////////////////////////// //
 // fiber data structures and functions //
 // /////////////////////////////////// //
 
 struct fiber_function_data_t {
-    job_manager_t *jm; // job manager
+    job_manager_handle jm; // job manager
 };
 
 // function executed by the fibers: will keep fetching and executing jobs
 void fiber_function(fcontext_transfer_t t) {
     struct fiber_function_data_t *fiber_data = (struct fiber_function_data_t*)t.data;
-    job_manager_t *jm = fiber_data->jm;
+    job_manager_handle jm = fiber_data->jm;
     while (jm && !jm->stop_threads) {
         printf("executing fiber function on thread %llx\n", (uint64_t)thrd_current());
         thrd_sleep(&(struct timespec){.tv_sec=1}, NULL);
@@ -46,7 +46,7 @@ void fiber_function(fcontext_transfer_t t) {
 // //////////////////////////////////// //
 
 struct thread_function_data_t {
-    job_manager_t *jm; // job manager
+    job_manager_handle jm; // job manager
 };
 
 // the thread function just does some housekeeping and then switches to the context of the fiber. 
@@ -72,8 +72,8 @@ int thread_function(void *data) {
 // job system //
 // ////////// //
 
-job_manager_t* create_job_manager(uint32_t num_worker_threads, uint32_t num_fibers, uint32_t fiber_stack_size) {
-    job_manager_t *jm = (job_manager_t*)malloc(sizeof(job_manager_t));
+job_manager_handle create_job_manager(uint32_t num_worker_threads, uint32_t num_fibers, uint32_t fiber_stack_size) {
+    job_manager_handle jm = (struct job_manager_t*)malloc(sizeof(struct job_manager_t));
     jm->threads = (thrd_t*)malloc(num_worker_threads*sizeof(thrd_t));
     jm->num_threads = num_worker_threads;
     jm->stop_threads = false;
@@ -86,19 +86,19 @@ job_manager_t* create_job_manager(uint32_t num_worker_threads, uint32_t num_fibe
     return jm;
 }
 
-void stop_job_manager(job_manager_t *jm) {
+void stop_job_manager(job_manager_handle jm) {
     jm->stop_threads = true;
     for (int i=0; i<jm->num_threads; i++) {
         thrd_join(jm->threads[i], NULL);
     }
 }
 
-void destroy_job_manager(job_manager_t **jm) {
+void destroy_job_manager(job_manager_handle jm) {
     // stop threads in case they're still running
-    if (!(*jm)->stop_threads) {
-        stop_job_manager(*jm);
+    if (!jm->stop_threads) {
+        stop_job_manager(jm);
     }
-    free(*jm);
-    *jm = NULL;
+    free(jm);
+    jm = NULL;
 }
 
